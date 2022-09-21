642998731069aaf48b640a3e607bbdf4c51be22a
binayachaudhary <binayasaurahwa@gmail.com>
2022-09-21 19:18:43 +0545

    fact: Redux- thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,18 +5,16 @@ import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategories } from '../../store/categories/category.action';
+import { fetchCategoriesAsyncs } from '../../store/categories/category.action';
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoriesAndDocuments('categories');
-      dispatch(setCategories(categoriesArray));
-    };
+    
+      // const categoriesArray = await getCategoriesAndDocuments('categories');
+      dispatch(fetchCategoriesAsyncs());
 
-    getCategoriesMap();
   }, []);
 
   return (
--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,27 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
+import { persistStore,persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
 
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
 
-  next(action);
+const persistConfig = {
+  key: 'root',
+  storage,
+  // blacklist: ['user']
+  whitelist: ['cart'],
+}
+const persistedReducer = persistReducer(persistConfig,rootReducer);
 
-  console.log('next state: ', store.getState());
-};
+const middleWares = [process.env.NODE_ENV !== 'production' &&  logger,thunk,].filter(Boolean);
 
-const middleWares = [loggerMiddleware];
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose ;
+const composedEnhancers =  composeEnhancer(applyMiddleware(...middleWares));
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const persistor =persistStore(store);
